937e4b931751f3a2d2c0ac1ec3ff41411c565833
Josh Pollmann <joshpollmann102@gmail.com>
2018-08-24 13:17:32 -0400

    Continue work on GUI

--- BookEStore/src/BookEStore.java
+++ BookEStore/src/BookEStore.java
@@ -1,7 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
@@ -9,13 +8,16 @@ import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
 /* Name: Joshua Pollmann
  * Course: CNT4714 - Fall 2018
- * Assignment title: Program 1 - Eent-driven Programming
+ * Assignment title: Program 1 - Event-driven Programming
  * Date: Sunday September 9, 2018
  */
-public class BookEStore extends Frame implements ActionListener 
+public class BookEStore extends JFrame implements ActionListener 
 {
+	
 	private Label nbrItemsLabel;
 	private Label bookIDLabel;
 	private Label quantityLabel;
@@ -42,8 +44,9 @@ public class BookEStore extends Frame implements ActionListener
 	
 	public BookEStore()
 	{
-		setLayout(new GridLayout());
-		topPanel = new Panel(new GridLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		topPanel = new Panel(new GridLayout(5,2));
 		bottomPanel = new Panel(new FlowLayout());
 		this.add(topPanel, BorderLayout.NORTH);
 		this.add(bottomPanel, BorderLayout.SOUTH);
@@ -76,17 +79,33 @@ public class BookEStore extends Frame implements ActionListener
 		
 		processItemBtn = new Button("Process Item #" + itemCount);
 		confirmItemBtn = new Button("Confirm Item #" + itemCount);
+		viewOrderBtn = new Button("View Order");
+		finishOrderBtn = new Button("Finish Order");
+		newOrderBtn = new Button("New Order");
+		exitBtn = new Button("Exit");
+		
+		bottomPanel.add(processItemBtn);
+		bottomPanel.add(confirmItemBtn);
+		bottomPanel.add(viewOrderBtn);
+		bottomPanel.add(finishOrderBtn);
+		bottomPanel.add(newOrderBtn);
+		bottomPanel.add(exitBtn);
+		
+		
+		setTitle("Book EStore");
+		setSize(550, 400);
+		setVisible(true);
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent arg0) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		new BookEStore();
 	}
 
